5c98f3fec3a175434910880433bece6a882791ef
LDC7 <lihwar1@gmail.com>
2019-12-27 20:07:08 +0400

    Стори комплитер. + лица.

--- ArtificialisExcogitatoris/CommandExecutor.cs
+++ ArtificialisExcogitatoris/CommandExecutor.cs
@@ -1,150 +1,171 @@
 ﻿namespace ArtificialisExcogitatoris
 {
-    using Discord;
-    using Discord.WebSocket;
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Threading.Tasks;
+  using System;
+  using System.Collections.Generic;
+  using System.IO;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using Discord;
+  using Discord.WebSocket;
 
-    public class CommandExecutor
+  public class CommandExecutor
+  {
+    private readonly DiscordSocketClient client;
+    private readonly ulong channelId;
+    private readonly ulong serverId;
+    private SocketMessage message;
+    private IReadOnlyCollection<GuildEmote> serverSmiles;
+    private Random random;
+
+    public CommandExecutor(DiscordSocketClient socketClient, ulong channelId, ulong serverId)
     {
-        private readonly DiscordSocketClient client;
-        private readonly ulong channelId;
-        private readonly ulong serverId;
-        private SocketMessage message;
-
-        public CommandExecutor(DiscordSocketClient socketClient, ulong channelId, ulong serverId)
-        {
-            this.client = socketClient;
-            this.channelId = channelId;
-            this.serverId = serverId;
-        }
-
-        public async Task ExecuteCommandAsync(SocketMessage socketMessage)
-        {
-            try
-            {
-                this.message = socketMessage;
-                var index = this.message.Content.IndexOf(' ');
-                var command = index != -1 ? this.message.Content.Substring(0, index) : this.message.Content;
-                switch (command)
-                {
-                    case "!help":
-                        await this.Help();
-                        break;
-                    case "!хокку":
-                        await this.Haiku();
-                        break;
-                    case "!facehelp":
-                        await this.Facehelp();
-                        break;
-                    default:
-                        if (this.Face(command)) break;
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                (this.message.Channel as IMessageChannel)?.SendMessageAsync($"Произошла ошибка: {ex.Message}");
-            }
-        }
-
-        private async Task Help()
-        {
-            const string helpMessage = "1) !help  - помощь\n" +
-                "2) !хокку - составить рандомное хокку\n" +
-                "3) !facehelp  - список 'лицевых' команд\n";
-
-            if (this.message.Channel is IMessageChannel messageChannel)
-            {
-                await messageChannel.SendMessageAsync(helpMessage);
-            }
-        }
-
-        private async Task Haiku()
-        {
-            if (this.message.Channel is IMessageChannel messageChannel)
-            {
-                var user = this.client
-                .GetGuild(this.serverId)
-                .Users
-                .Where(u => u.Status == UserStatus.Online)
-                .OrderBy(u => Guid.NewGuid())
-                .First();
-
-                var name = string.IsNullOrWhiteSpace(user.Nickname) ? user.Username : user.Nickname;
-
-                await messageChannel.SendMessageAsync(HaikuGenerator.Generate(name));
-            }
-        }
-
-        private async Task Facehelp()
-        {
-            if (this.message.Channel is IMessageChannel messageChannel)
-            {
-                await messageChannel.SendMessageAsync(FaceWorker.GetFacesDescriptions());
-            }
-        }
-
-        private bool Face(string command)
-        {
-            if (!command.Contains("face"))
-                return false;
-
-            if (this.message.Channel is IMessageChannel messageChannel)
-            {
-                Tuple<Stream, string> result = null;
-                string url = string.Empty;
-
-                if (this.message.Attachments.Count == 1)
-                {
-                    var attachment = this.message.Attachments.FirstOrDefault();
-                    if (attachment != null)
-                    {
-                        url = attachment.Url;
-                    }
-                }
-                else
-                {
-                    url = this.message.Content.Substring(this.message.Content.IndexOf(' ') + 1);
-                }
-
-                if (command == "!face")
-                {
-                    result = FaceWorker.AddFaceFromUrl(url);
-                }
-                else
-                {
-                    result = FaceWorker.AddFaceFromUrl(url, command.Substring(1));
-                }
-
-                if (result != null)
-                {
-                    this.SendImage(messageChannel, result.Item1, result.Item2);
-                }
-                else
-                {
-                    messageChannel.SendMessageAsync("У меня не получилось...");
-                }
-            }
-
-            return true;
-        }
-
-        private async void SendImage(IMessageChannel messageChannel, Stream stream, string filename = "file.png")
-        {
-            try
-            {
-                if (messageChannel != null && stream != null)
-                {
-                    await messageChannel.SendFileAsync(stream, filename);
-                }
-            }
-            finally
-            {
-                stream.Dispose();
-            }
-        }
+      this.client = socketClient;
+      this.channelId = channelId;
+      this.serverId = serverId;
+      this.random = new Random();
     }
+
+    public async Task ExecuteCommandAsync(SocketMessage socketMessage)
+    {
+      try
+      {
+        this.message = socketMessage;
+        var index = this.message.Content.IndexOf(' ');
+        var command = index != -1 ? this.message.Content.Substring(0, index) : this.message.Content;
+        switch (command)
+        {
+          case "!help":
+            await this.Help();
+            break;
+          case "!хокку":
+            await this.Haiku();
+            break;
+          case "!facehelp":
+            await this.Facehelp();
+            break;
+          case "!story":
+            var storyBegining = index != -1 ? this.message.Content.Substring(index + 1) : null;
+            await this.MakeStory(storyBegining);
+            break;
+          default:
+            if (this.Face(command)) break;
+            break;
+        }
+      }
+      catch (Exception ex)
+      {
+        (this.message.Channel as IMessageChannel)?.SendMessageAsync($"Произошла ошибка: {ex.Message}");
+      }
+    }
+
+    private async Task Help()
+    {
+      const string helpMessage = "1) !help  - помощь\n" +
+        "2) !хокку - составить рандомное хокку\n" +
+        "3) !facehelp  - список 'лицевых' команд\n" +
+        "4) !story - дополнить историю\n";
+
+      if (this.message.Channel is IMessageChannel messageChannel)
+      {
+        await messageChannel.SendMessageAsync(helpMessage);
+      }
+    }
+
+    private async Task Haiku()
+    {
+      if (this.message.Channel is IMessageChannel messageChannel)
+      {
+        var user = this.client
+        .GetGuild(this.serverId)
+        .Users
+        .Where(u => u.Status == UserStatus.Online)
+        .OrderBy(u => Guid.NewGuid())
+        .First();
+
+        var name = string.IsNullOrWhiteSpace(user.Nickname) ? user.Username : user.Nickname;
+
+        await messageChannel.SendMessageAsync(HaikuGenerator.Generate(name));
+      }
+    }
+
+    private async Task Facehelp()
+    {
+      if (this.message.Channel is IMessageChannel messageChannel)
+      {
+        await messageChannel.SendMessageAsync(FaceWorker.GetFacesDescriptions());
+      }
+    }
+
+    private bool Face(string command)
+    {
+      if (!command.Contains("face"))
+        return false;
+
+      if (this.message.Channel is IMessageChannel messageChannel)
+      {
+        Tuple<Stream, string> result = null;
+        string url = string.Empty;
+
+        if (this.message.Attachments.Count == 1)
+        {
+          var attachment = this.message.Attachments.FirstOrDefault();
+          if (attachment != null)
+          {
+            url = attachment.Url;
+          }
+        }
+        else
+        {
+          url = this.message.Content.Substring(this.message.Content.IndexOf(' ') + 1);
+        }
+
+        if (command == "!face")
+        {
+          result = FaceWorker.AddFaceFromUrl(url);
+        }
+        else
+        {
+          result = FaceWorker.AddFaceFromUrl(url, command.Substring(1));
+        }
+
+        if (result != null)
+        {
+          this.SendImage(messageChannel, result.Item1, result.Item2);
+        }
+        else
+        {
+          messageChannel.SendMessageAsync("У меня не получилось...");
+        }
+      }
+
+      return true;
+    }
+
+    private async void SendImage(IMessageChannel messageChannel, Stream stream, string filename = "file.png")
+    {
+      try
+      {
+        if (messageChannel != null && stream != null)
+        {
+          await messageChannel.SendFileAsync(stream, filename);
+        }
+      }
+      finally
+      {
+        stream.Dispose();
+      }
+    }
+
+    private async Task MakeStory(string begining)
+    {
+      if (!string.IsNullOrWhiteSpace(begining) && this.message.Channel is IMessageChannel messageChannel)
+      {
+        if (this.serverSmiles == null)
+          this.serverSmiles = this.client.GetGuild(this.serverId).Emotes;
+
+        var smile = this.serverSmiles.ElementAt(this.random.Next(0, this.serverSmiles.Count()));
+        await messageChannel.SendMessageAsync($"{StoryMaker.Complete(begining)} {smile.ToString()}");
+      }
+    }
+  }
 }
---
+++ ArtificialisExcogitatoris/StoryMaker.cs
@@ -0,0 +1,50 @@
+﻿namespace ArtificialisExcogitatoris
+{
+  using System.IO;
+  using System.Linq;
+  using System.Net;
+  using System.Text;
+  using Newtonsoft.Json.Linq;
+
+  internal class StoryMaker
+  {
+    const string URL = @"https://models.dobro.ai/gpt2/medium/";
+
+    internal static string Complete(string begining)
+    {
+      string fullStory = begining;
+      WebRequest request = WebRequest.Create(URL);
+      request.Method = "POST";
+      request.ContentType = "text/plain;charset=UTF-8";
+      request.Timeout = 10000;
+
+      byte[] byteArray = CreateRequestPayload(begining);
+      request.ContentLength = byteArray.Length;
+
+      using (Stream dataStream = request.GetRequestStream())
+      {
+        dataStream.Write(byteArray, 0, byteArray.Length);
+      }
+
+      WebResponse response = request.GetResponseAsync().Result;
+      using (Stream stream = response.GetResponseStream())
+      {
+        using (StreamReader reader = new StreamReader(stream))
+        {
+          JObject jsonResponse = JObject.Parse(reader.ReadToEnd());
+          JArray answers = (JArray)jsonResponse["replies"];
+          fullStory += answers.FirstOrDefault()?.ToString();
+        }
+      }
+      response.Close();
+
+      return fullStory;
+    }
+
+    private static byte[] CreateRequestPayload(string prompt)
+    {
+      string payload = "{ \"prompt\": \"" + prompt + "\", \"length\": 30, \"num_samples\": 1}";
+      return Encoding.UTF8.GetBytes(payload);
+    }
+  }
+}
